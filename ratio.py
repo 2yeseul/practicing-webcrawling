3068372eff24085defc8abac392f62c86cc9f9c1
2yeseul <43533905+2yeseul@users.noreply.github.com>
2018-10-01 22:45:54 +0900

    Update ratio.py
    내일 할 것 div 배열 만들기. for 문 만들어서 각 div값에 대한 td값 찾아내기

--- ratio.py
+++ ratio.py
@@ -3,62 +3,107 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-#response = requests.get('http://addon.jinhakapply.com/RatioV1/RatioH/Ratio11080161.html?1537707368320')
+# response = requests.get('http://addon.jinhakapply.com/RatioV1/RatioH/Ratio11080161.html?1537707368320')
 
 # 크롤링하고자 하는 url
 targetUrl = "http://addon.jinhakapply.com/RatioV1/RatioH/Ratio11080161.html?1537707368320"
 # targelUrl의 코드를 읽어온다
-#html = urlopen(targetUrl).read()
+# html = urlopen(targetUrl).read()
 # beautifulSoup을 통하여 html로 parsing
 soupData = BeautifulSoup(urlopen(targetUrl).read(), 'html.parser')
 
 sel = soupData.find('select', {'id': 'selType'})
 
 optionVal = list(soupData.find_all('option'))
-#optionVal = sel['value']
+# optionVal = sel['value']
 selList = []
-
-
+st = 'SelType'
 # 전형 selValue 값 저장 in selList
 for o in optionVal:
     val = o.get('value')
+    newst = st+val
     if '0' not in val:
-        selList.append([val])
+        selList.append([newst])
+hList = []
+h2 = soupData.find_all('h2')
 
 
-idList = []
-divs = []
+table = []
 
-div = soupData.find('div')
-for d in div:
-    dd = d.get('id')
-    divs.append([dd])
-print(divs)
+td = soupData.find_all('td')
+
+tb = soupData.find_all('table',{'class': 'tableRatio3'})
+
+test = []
+    #if htxt in selList:
+
+        # for j in td:
+         #   tdText = j.get_text()
+          #  print(tdText)
+        #tdText = td.get_text()
 
 
+# for i in selList:
+ #   print(i)
+
+# for i in range(0,len(selList)):
+divArr = []
+divs = soupData.find_all('div')
+#td = divs.find_all('td')
+newID = []
+tdList = []
+for i in divs:
+    idName = i.get('id')
+    newID.append(idName)
+    idValue = idName.select(selList[0])
+    print(idValue)
+
+#    for j in range(0,len(selList)) :
+
+#    for j in range(0,len(selList)):
+#        if i.get('id') in selList[j]:
+#            for h in i.
+    # print(idName)
+    # for j in range(0,len(selList)):
+
+
+#    id = i.get('id')
+#    newID.append([id])
+#    tds = i.find_all('td')
+#    print(tds)
+    #for j in range(0, len(selList)):
+    #    if selList[j] in newID:
+    #       print(tds)
+           #print(i.find_all('td'))
+
+#    for j in range(0, len(selList)):
+#        if selList[j] in i.find('id'):
+#            print(selList[j])
+
+
+
+# for d in div:
+#    id = d.get('id')
+#    divs.append([id])
+#    for s in range(0, len(selList)):
+
+# for s in selList:
+ #   print(s)
 #   idList.append([divs])
 
 
 # for i in range(0, len(selList)):
 
 
-
-#for opt in optionVal:
+# for opt in optionVal:
 #    selType = opt.text
 #    selList.append([selType])
-#print(selList)
+# print(selList)
 
-#for sel in selList:
-
-
-
-
-
+# for sel in selList:
 
 
 # for i in listSel:
-
-
 
 
 '''
@@ -71,8 +116,8 @@ print(divs)
             '''
 
 # print(data)
-            
-         
+
+
 '''
             displayName = tds[0].text
             mojip = tds[1].text
