bd084d1c622317c32167b04746841d67f52fc17c
2yeseul <43533905+2yeseul@users.noreply.github.com>
2018-10-05 14:36:03 +0900

    Update ratio.py

--- ratio.py
+++ ratio.py
@@ -1,107 +1,39 @@
 from urllib.request import urlopen
-import requests
 from bs4 import BeautifulSoup
-import re
 
-# response = requests.get('http://addon.jinhakapply.com/RatioV1/RatioH/Ratio11080161.html?1537707368320')
+
 
 # 크롤링하고자 하는 url
 targetUrl = "http://addon.jinhakapply.com/RatioV1/RatioH/Ratio11080161.html?1537707368320"
-# targelUrl의 코드를 읽어온다
-# html = urlopen(targetUrl).read()
-# beautifulSoup을 통하여 html로 parsing
+
 soupData = BeautifulSoup(urlopen(targetUrl).read(), 'html.parser')
 
-sel = soupData.find('select', {'id': 'selType'})
-
-optionVal = list(soupData.find_all('option'))
-# optionVal = sel['value']
-selList = []
-st = 'SelType'
-# 전형 selValue 값 저장 in selList
-for o in optionVal:
-    val = o.get('value')
-    newst = st+val
-    if '0' not in val:
-        selList.append([newst])
-
-
-
+# 크롤링으로 얻을 data 저장
 data = []
+# div 태그를 가진 코드를 전부 찾는다
+divs = soupData.find_all("div")
 
+# 각 div 태그 안, tr과 td 찾기
+for div in divs:
+    for tr in soupData.find_all("tr",{"onmouseover" : "this.className='over';"}):
+        tds = list(tr.find_all("td"))
+        for td in tds:
+            # 단과대학 포함된 경우
+            if '대학' in tds[0].text.strip() and len(tds) >=5:
+                displayName = tds[1].text.strip()
+                mojip = tds[2].text.strip()
+                jiwon = tds[3].text.strip()
+                ratio = tds[4].text.strip()
 
-for i in soupData.find_all('table', {'class' : 'tableRatio3'}):
-    for j in i.find_all('tr'):
-        tds = list(j.find_all('td'))
-       # for td in tds:
-       #     print("")
+            # 단과대학 포함되지 않은 경우
+            elif '대학' not in tds[0].text.strip() and len(tds) >= 4:
+                displayName = tds[0].text.strip()
+                mojip = tds[1].text.strip()
+                jiwon = tds[2].text.strip()
+                ratio = tds[3].text.strip()
 
+        data.append([displayName, mojip, jiwon, ratio ])
 
-            #print(tds[0].text)
-            #displayName = td[0].text
-            #print(displayName)
-
-'''
-            if '대학' not in tds:
-                displayName = tds[0].text
-                mojip = tds[1].text
-                jiwon = tds[2].text
-                ratio = tds[3].text
-                data.append([mojip, jiwon, ratio])
-'''
-
-
-
-
-###for tr in table.find('tr'):
-###    print(tr)
-
-
-
-'''
-        if '대학' in tds[0].text:
-            displayName = tds[1].text
-            mojip = tds[2].text
-            jiwon = tds[3].text
-            ratio = tds[4].text
-            data.append([displayName, mojip, jiwon, ratio])
-            '''
-
-# print(data)
-
-
-'''
-            displayName = tds[0].text
-            mojip = tds[1].text
-            jiwon = tds[2].text
-            ratio = tds[3].text
-            data.append([displayName, mojip, jiwon, ratio])
-            print(data)
-'''
-
-'''
-# 학과명을 받아오는 변수
-#unit = soupData
-# 경쟁률을 받아오는 변수
-rate = soupData
-
-#p = re.compile()
-unit = soupData.findAll("td", {"class":"unit"})
-
-rspn = unit.find("rowspan")
-for tag in unit:
-    if rspn in tag:
-        print(tag)
-        tag.extract()
-    print(tag)
-    
-'''
-# for rowspan in rowspanElements:
- #   rowspan.extract()
-
-
-# for tag in unit.select('td[class=unit]'):
-#    unitList = tag.text.strip()
-#    if '대학' not in unitList:
-#        print(unitList)
-
+for i in range(0, len(data)):
+    print(data[i])
+#print(data)
